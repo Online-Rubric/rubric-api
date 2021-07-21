0562b9c1436609162ab22261e18692a5a4275692
Brian Lemons <brian_lemons@hotmail.com>
2021-07-21 16:40:30 -0700

    adding user and proctor view and url

--- rubrics/urls.py
+++ rubrics/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import RubricList, RubricDetail
+from .views import RubricList, RubricDetail, StudentList, ProctorList
 
 urlpatterns = [
-    path("", RubricList.as_view(), name="Rubric_list"),
+    path("", RubricList.as_view(), name="Rubric_list"), 
     path("<int:pk>/", RubricDetail.as_view(), name="Rubric_detail"),
+    path("students/", StudentList.as_view(), name="Student_detail"),
+    path("proctors/", ProctorList.as_view(), name="Proctor_detail"),
 ]
