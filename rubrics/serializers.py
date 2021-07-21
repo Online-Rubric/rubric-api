d296a7072e850b2d15fbce1a86bc09231e69ca64
Timothy <Timothy.viccari@gmail.com>
2021-07-21 17:30:00 -0400

    update model to add defaults, add new views and serializers for student and proctor

--- rubrics/serializers.py
+++ rubrics/serializers.py
@@ -1,4 +1,6 @@
 
+from django.contrib.auth import get_user_model
+
 from rest_framework import serializers
 
 from .models import Rubric
@@ -7,4 +9,15 @@ from .models import Rubric
 class RubricSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rubric
+        fields = "__all__"
+        read_only_fields = ["total_score", "percent"]
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = "__all__"
+
+class ProctorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
         fields = "__all__"
--- rubrics/views.py
+++ rubrics/views.py
@@ -1,10 +1,14 @@
+from django.contrib.auth import get_user_model
+from django.db.models import query
 from rest_framework.generics import (
     ListCreateAPIView,
     RetrieveUpdateDestroyAPIView,
+    ListAPIView
 )
 from .models import Rubric
 from .permissions import IsStudentOrReadOnly
-from .serializers import RubricSerializer
+from .serializers import RubricSerializer, StudentSerializer, ProctorSerializer
+from django.contrib.auth import get_user_model
 
 
 class RubricList(ListCreateAPIView):
@@ -16,3 +20,11 @@ class RubricDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = (IsStudentOrReadOnly,)
     queryset = Rubric.objects.all()
     serializer_class = RubricSerializer
+
+class StudentList(ListAPIView):
+    queryset = get_user_model().objects.filter(is_staff=False)
+    serializer_class = StudentSerializer
+
+class ProctorList(ListAPIView):
+    queryset = get_user_model().objects.filter(is_staff=True)
+    serializer_class = ProctorSerializer
